d49da5c20282906a27c582865a5cb6418c9dfd51
Eric Berry <cavneb@gmail.com>
2018-01-22 09:03:35 -0700

    Add Property Model

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.models import Sponsor
+from app.models import Sponsor, Property
 
 # Register your models here.
 admin.site.register(Sponsor)
+admin.site.register(Property)
