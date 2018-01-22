a14a1bc90f450d17a218cfed7e46e652fef136e7
Eric Berry <coderberry@gmail.com>
2018-01-22 15:23:03 -0700

    Add pixel-based impression tracking (#23)

--- track/urls.py
+++ track/urls.py
@@ -3,5 +3,6 @@ from track import views
 
 app_name = 'track'
 urlpatterns = [
+    url(r'^l/(?P<token>[\w\-]+)/pixel.png$', views.pixel, name='pixel'),
     url(r'^c/(?P<token>[\w\-]+)/$', views.track_click, name='track_click'),
 ]
--- track/views.py
+++ track/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.conf import settings
 
 from app.models import Sponsorship
-from .models import Click
+from .models import Click, Impression
+
+
+def pixel(request, token):
+    try:
+        sponsorship = Sponsorship.objects.get(token=token)
+    except Sponsorship.DoesNotExist:
+        sponsorship = None
+
+    user_agent = request.META.get('HTTP_USER_AGENT', None)
+    referer = request.META.get('HTTP_REFERER', 'None')
+    ip_address = get_client_ip(request)
+
+    if sponsorship:
+        if ip_address:
+            print("IP ADDRESS: {0}".format(ip_address))
+
+            impression = Impression(sponsorship=sponsorship)
+
+            # Denormalize for speed of query
+            impression.property_id = sponsorship.property_id
+            impression.sponsor_id = sponsorship.sponsor_id
+            impression.user_agent = user_agent
+            impression.ip_address = ip_address
+
+            impression.is_bot = False  # TODO
+
+            impression.save()
+
+    BASE_DIR = getattr(settings, "BASE_DIR", None)
+    image_data = open(BASE_DIR + "/static/images/pixel.png", "rb").read()
+    return HttpResponse(image_data, content_type="image/png")
 
 
 def track_click(request, token):
@@ -18,7 +50,7 @@ def track_click(request, token):
     if sponsorship:
         if ip_address:
             print("IP ADDRESS: {0}".format(ip_address))
-            
+
             click = Click(sponsorship=sponsorship)
 
             # Denormalize for speed of query
