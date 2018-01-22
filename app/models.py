69c8ad1b44a507962c2b4f6c63426daf72383a42
Eric Berry <cavneb@gmail.com>
2018-01-22 08:53:35 -0700

    Add Sponsor model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Sponsor(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Sponsor, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
