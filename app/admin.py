d14f035d92cd066d81e687c41fb694bd1cfb4f7a
Eric Berry <coderberry@gmail.com>
2018-01-22 17:17:20 -0700

    Clean up admin views (fixes #26) (#27)

--- app/admin.py
+++ app/admin.py
@@ -2,12 +2,36 @@ from django.contrib import admin
 from app.models import Sponsor, Property, Sponsorship
 
 
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    ordering = ['name']
+    list_filter = ['name', 'slug']
+    search_fields = ['name', 'slug']
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url']
+    ordering = ['name']
+    list_filter = ['name', 'url']
+    search_fields = ['name', 'url']
+
+
 class SponsorshipAdmin(admin.ModelAdmin):
-    exclude = ('token',)
-    readonly_fields = ('token',)
+    exclude = ['token']
+    readonly_fields = ('token', )
+    list_display = ['redirect_url', 'token']
+    ordering = ['redirect_url']
+    list_filter = [
+        'sponsor__name', 'sponsor__slug', 'property__name', 'property__url',
+        'token'
+    ]
+    search_fields = [
+        'sponsor__name', 'sponsor__slug', 'property__name', 'property__url',
+        'token'
+    ]
 
 
 # Register your models here.
-admin.site.register(Sponsor)
-admin.site.register(Property)
+admin.site.register(Sponsor, SponsorAdmin)
+admin.site.register(Property, PropertyAdmin)
 admin.site.register(Sponsorship, SponsorshipAdmin)
