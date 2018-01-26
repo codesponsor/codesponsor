2d49e6976d5dea4899df205cc3a278c725bba343
Mark Beacom <mark.beacom@cirrustack.com>
2018-01-26 17:41:57 -0500

    Modify celery vars to use CELERY_ namespace

--- code_sponsor/celery.py
+++ code_sponsor/celery.py
@@ -7,9 +7,6 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_sponsor.settings')
 
-app = Celery(
-    'code_sponsor',
-    broker=settings.BROKER_URL,
-    backend=settings.BROKER_URL)
+app = Celery('code_sponsor')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
