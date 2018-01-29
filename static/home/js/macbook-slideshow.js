10a00b36f96e35faf8ee65660bc35d5badf95c9c
Tommy Cox <drupal.tom@gmail.com>
2018-01-29 01:06:45 -0700

    Removed unnecessary "active" class

--- static/home/js/macbook-slideshow.js
+++ static/home/js/macbook-slideshow.js
@@ -4,8 +4,6 @@ var slideInterval = setInterval(nextSlide,3500);
 
 function nextSlide() {
     $(slides[currentSlide]).hide();
-    $(slides[currentSlide]).removeClass('showing');
     currentSlide = (currentSlide+1)%slides.length;
     $(slides[currentSlide]).fadeIn();
-    $(slides[currentSlide]).addClass('showing');
 }
