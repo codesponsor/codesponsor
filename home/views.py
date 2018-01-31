0677dae6a26400b6b508334fb14e1410f8cf75ab
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-01-31 00:23:02 -0500

    Add mail method to use ContactForm

--- home/views.py
+++ home/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .forms import ContactForm
 
 def index(request):
-    return render(request, 'index.html', context={})
+    if request.method == 'GET':
+        form = ContactForm()
+
+    return render(request, 'index.html', {'form': form})
+
+def mail(request):
+    '''Send email via mailgun'''
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            content = form.cleaned_data['content']
+
+            recipients = ['ken.u.diggitt@gmail.com']
+
+            send_mail(subject, content, f"{name} <{email}>", recipients)
+            messages.success(request, 'Success!')
+            return HttpResponseRedirect('/')
+    else:
+        form = ContactForm()
+
+    return render(request, 'index.html', {'form': form})
