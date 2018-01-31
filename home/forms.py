bdff542a63f5a890c2d228ddd164a7aaa771f9d6
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-01-31 12:20:27 -0500

    Add new line before class and add space to trailing commas

--- home/forms.py
+++ home/forms.py
@@ -1,29 +1,30 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
     name = forms.CharField(
         required=True,
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Name',}))
+                'placeholder': 'Name', }))
 
     email = forms.EmailField(
         required=True,
         widget=forms.EmailInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Enter Email',}))
+                'placeholder': 'Enter Email', }))
 
     subject = forms.CharField(
         required=True,
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Your Subject...',}))
+                'placeholder': 'Your Subject...', }))
 
     content = forms.CharField(
         widget=forms.Textarea(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Your message...',}))
+                'placeholder': 'Your message...', }))
