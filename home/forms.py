3b88b0cf5e73b59291ab71e4b4303d45f9ebcd94
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-01-31 00:22:48 -0500

    Add custom ContactForm class

---
+++ home/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Name',}))
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Email',}))
+
+    subject = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Your Subject...',}))
+
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Your message...',}))
